1f332778d78a27fadd933337d4048679eed6129f
Azeez Akinsola <harkzed@gmail.com>
2019-07-19 17:56:53 -1200

    working on the url

--- products/urls.py
+++ products/urls.py
@@ -2,30 +2,31 @@
 from django.urls import path
 
 from .views import (
-    ProductDetailView, 
-    ProductListView, 
-    product_detail_view, 
-    product_list_view,
-    ProductFeaturedListView,
-    ProductFeaturedDetailView,
+    ProductListView,
     ProductDetailSlugView,
+
+
+    # ProductFeaturedDetailView,
+    # ProductDetailView,    
+    # product_detail_view, 
+    # product_list_view,
+    # ProductFeaturedListView,
+    
 
     )
 
 
 urlpatterns = [
  
-    path('products/', ProductListView.as_view()),
-    path('products-fbv/', product_list_view),
+    path('', ProductListView.as_view()),
+    path('<str:slug>/', ProductDetailSlugView.as_view()),
 
-    # path('products/<int:pk>/', ProductDetailView.as_view()),
-    path('products/<str:slug>/', ProductDetailSlugView.as_view()),
 
-    path('products-fbv/<int:pk>/', product_detail_view),
-    # path('products-fbv/<str:slug>/', product_detail_view),
-
-    path('featured/', ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
+    # path('products-fbv/', product_list_view),
+    # path('products-fbv/<int:pk>/', product_detail_view),
+    # # path('products-fbv/<str:slug>/', product_detail_view),
+    # path('featured/', ProductFeaturedListView.as_view()),
+    # path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
 
 ]
 
